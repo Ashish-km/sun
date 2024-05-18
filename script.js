f82f2549ef7e2b4b0057db9a17144cec48bc61d9
Ashish Kushwah <ashishkushwah88@gmail.com>
2024-05-18 15:57:25 +0530

    update

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-var tl = gsap.timeline()
-tl.form("nav",{
-  opacity:0,
-  duration:1,
-  delay:0.5
-})
+var tl = gsap.timeline();
+tl.from("nav", {
+  opacity: 0,
+  duration: 1,
+  delay: 0.5
+});
+tl.from("herotext", {
+  opacity: 0,
+  duration: 1,
+  delay: 0.5
+});
